5f1f25ba41a1604c76f45dacf7976afdd28ba3c7
Andre Sionek <andresionek91@gmail.com>
2020-10-23 00:20:19 +0100

    cria testes

--- turma_2/2_integracao_continua_e_testes/4.testes/s3_stack/s3_stack.py
+++ turma_2/2_integracao_continua_e_testes/4.testes/s3_stack/s3_stack.py
@@ -9,5 +9,4 @@ class S3Stack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        encryption = s3.BucketEncryption(s3.BucketEncryption.S3_MANAGED)
-        s3.Bucket(self, 'meu-bucket-belisco-cdk', bucket_name='meu-bucket-belisco-cdk', encryption=encryption)
+        s3.Bucket(self, 'meu-bucket-belisco-cdk-testes', bucket_name='meu-bucket-belisco-cdk-testes')
